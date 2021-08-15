d0dabdd4cf935d1308bf53e39d927c5528f75e9d
Vlad Safin <djutsu17@gmail.com>
2021-08-15 22:42:40 +0300

    complete all tasks

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goit-nodejs-hw-01-CLI-Application
+
+https://prnt.sc/1pa2y83
+https://prnt.sc/1pa51mg
+https://prnt.sc/1pa9259
+https://prnt.sc/1paec96
---
+++ contacts.js
@@ -0,0 +1,74 @@
+const fs = require("fs/promises")
+const path = require("path")
+const { v4 } = require("uuid")
+
+const contactsPath = path.join(__dirname, "db/contacts.json")
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath)
+    const contactsList = JSON.parse(data)
+    return contactsList
+  } catch (error) {
+    throw error
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contactsList = await listContacts()
+    const selectContact = contactsList.find(
+      (item) => item.id === Number(contactId)
+    )
+
+    if (!selectContact) {
+      throw new Error(`Contact with id=${contactId} not found`)
+    }
+
+    return selectContact
+  } catch (error) {
+    throw error
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contactsList = await listContacts()
+    const contactsIndex = contactsList.findIndex(
+      (item) => item.id === Number(contactId)
+    )
+
+    if (contactsIndex === -1) {
+      throw new Error(`Contact with id=${contactId} not found`)
+    }
+
+    const removedContact = await contactsList.splice(contactsIndex, 1)
+    await fs.writeFile(contactsPath, JSON.stringify(contactsList))
+
+    return removedContact
+  } catch (error) {
+    throw error
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contactsList = await listContacts()
+    const addNewContact = { id: v4(), name, email, phone }
+
+    contactsList.push(addNewContact)
+
+    await fs.writeFile(contactsPath, JSON.stringify(contactsList))
+
+    return addNewContact
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+}
